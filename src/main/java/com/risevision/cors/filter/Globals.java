cad0a76a4a3e6c143deeacb06004be12ebd91c91
santiagonoguez <santiago@nul-unu.com>
2019-12-16 08:50:05 -0600

    fix indents, fix PMD corrections

--- src/main/java/com/risevision/cors/filter/Globals.java
+++ src/main/java/com/risevision/cors/filter/Globals.java
@@ -2,11 +2,11 @@ package com.risevision.cors.filter;
 
 public interface Globals {
 
-  final String ALLOWED_CREDENTIALS_HEADER = "Access-Control-Allow-Credentials";
-  final String ALLOWED_METHODS_HEADER = "Access-Control-Allow-Methods";
-  final String ALLOWED_ORIGIN_HEADER = "Access-Control-Allow-Origin";
-  final String ORIGIN_HEADER = "origin";
+  String ALLOWED_CREDENTIALS_HEADER = "Access-Control-Allow-Credentials";
+  String ALLOWED_METHODS_HEADER = "Access-Control-Allow-Methods";
+  String ALLOWED_ORIGIN_HEADER = "Access-Control-Allow-Origin";
+  String ORIGIN_HEADER = "origin";
 
-  final String ALLOWED_ORIGINS_PARAM = "allow-origins";
+  String ALLOWED_ORIGINS_PARAM = "allow-origins";
 
 }
